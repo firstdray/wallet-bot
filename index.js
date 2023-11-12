dcdc1d74ae9621ae34bac69ba42fed19e9d4e026
firstdray <142772338+firstdray@users.noreply.github.com>
2023-11-13 02:04:09 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
-const { Telegraf } = require('telegraf')
+const { Markup, Telegraf } = require('telegraf')
 const etherBalance = require("./function/command.js")
 
-const bot = new Telegraf('сюда пишем API бота')
+
+const bot = new Telegraf('сюда API бота')
 bot.command('balance', async (ctx) => {
-    const balance = await etherBalance.getBalance()
-    ctx.reply(`${balance} ETH`)
+    ctx.reply(
+        'Выбирите сеть',
+        Markup.inlineKeyboard([
+            Markup.button.callback("polygon", "polygon"),
+            Markup.button.callback('arbitrum', 'arbitrum'),
+            Markup.button.callback('bsc', 'bsc'
+        )]))
+    bot.action('polygon', async (ctx) => {
+        const network = "https://polygon-mumbai-pokt.nodies.app"
+        const address = "свой адрес кошелька"
+        const balance = await etherBalance.getBalance(network, address)
+        ctx.reply(`${balance} Matic`)
+    })
+    bot.action('arbitrum', async (ctx) => {
+        const network = "https://arb-mainnet-public.unifra.io"
+        const address = "свой адрес кошелька"
+        const balance = await etherBalance.getBalance(network, address)
+        ctx.reply(`${balance} ETH`)
+    })
+    bot.action('bsc', async (ctx) => {
+        const network = "https://binance.llamarpc.com"
+        const address = "свой адрес кошелька"
+        const balance = await etherBalance.getBalance(network, address)
+        ctx.reply(`${balance} BNB`)
+    })
 })
+
 bot.command('balancetoken', async (ctx) => {
-    let balanceToken = await etherBalance.getBalanceERC20()
-    let erc20name = await etherBalance.getERC20name()
+    ctx.reply('Введите Адресс Контракта токена')
+    bot.on('text', async (ctx) => {
+        const ERC20 = ctx.message.text
+    let balanceToken = await etherBalance.getBalanceERC20(ERC20)
+    let erc20name = await etherBalance.getERC20name(ERC20)
     ctx.reply(`${balanceToken} ${erc20name}`)
+})
 })
 bot.command('sendeth', async (ctx) => {
     let transaction = await etherBalance.sendEth()
     ctx.reply(`${transaction}`)
 })
 bot.command('sendtoken', async (ctx) => {
-    let transaction = await etherBalance.sendErc20()
-    let erc20name = await etherBalance.getERC20name()
+    const transaction = await etherBalance.sendErc20()
+    const erc20name = await etherBalance.getERC20name()
     ctx.reply(`${transaction} ${erc20name}`)
 })
 
